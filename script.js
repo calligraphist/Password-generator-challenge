da7edcb8de1736fb2f71d70513ccb14a2f3c8d90
“anar <sehar.uzair23@gmail.com>
2023-02-02 16:46:45 -0400

    fixed declaration

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var lowerCase= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
-var selectedArray = [];
+var expectedOutcome = [];
 function generatePassword(){
 // asking user to choose password length using if-else statement
 numberOfCharacters = prompt("Please select your password length between 8-128 characters.");
@@ -62,7 +62,7 @@ if (choiceOfspecialChar) {
   expectedOutcome = expectedOutcome.concat(choiceOfspecialChar);
 }
 //
-var selectedArray = "";
+var finalPassword = "";
 for (var i = 0; i < numberOfCharacters; i++) {
   var random =Math.floor(Math.random() * expectedOutcome.length);
    finalPassword += expectedOutcome[random];
