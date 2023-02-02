f0a28267d632bdad79d2071b8a3d3dd212416b14
“anar <sehar.uzair23@gmail.com>
2023-02-02 10:37:29 -0400

    variables defined

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// declaring variable and assigning values in array and append the set of blank array
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+var selectedArray = [];
+function generatePassword(){
+// asking user to choose password length using if-else statement
+numberOfCharacters = prompt("Please select your password length between 8-128 characters.");
+if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+  //return "Please choose a valid number of characters.";
+} else if (isNaN(numberOfCharacters)) {
+  numberOfCharacters = prompt("Not a valid number, Please enter a valid number.");
+}
+else {
+  alert("The length of your password will be " + numberOfCharacters + " characters.");
+  
+}
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
